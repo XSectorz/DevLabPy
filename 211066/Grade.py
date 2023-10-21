f9bef832abee237e4913c0864dabdc0028ef79e2
XSectorz <panatschool@gmail.com>
2023-10-21 16:23:51 +0700

    First upload

---
+++ 211066/Grade.py
@@ -0,0 +1,13 @@
+
+score = int(input())
+
+if(score>=80):
+    print("A")
+elif(score>= 70 and score<= 79):
+    print("B")
+elif(score>= 60 and score<= 69):
+    print("C")
+elif(score>=50 and score <= 59):
+    print("D")
+else:
+    print("F")
---
+++ 211066/MinOrMax.py
@@ -0,0 +1,10 @@
+
+a = int(input())
+b = int(input())
+
+if(a > b):
+    print("A")
+elif(b>a):
+    print("B")
+else:
+    print("AB")
